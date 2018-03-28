21cc08a79720e0017ee09969b670d7e29fdefeab
Rin Ng <Rin08.ng@gmail.com>
2018-03-28 15:48:41 +0700

    Added hw9 of week2

---
+++ reverseWords.js
@@ -0,0 +1,20 @@
+function balikKata(kata) {
+  var index = kata.length - 1;
+  var temp= "";
+
+  while(index >= 0){
+    temp = temp + kata[index];
+    index--;
+  }
+
+
+  return temp;
+
+}
+
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
